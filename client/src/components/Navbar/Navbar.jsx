139b04ca258fb689d7f673e36b750190cac223bd
ayushjrathod <clashofa1057@gmail.com>
2024-09-27 20:37:50 +0530

    navbar changes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -11,11 +11,11 @@ function Navbar() {
         </a>
         <div className="flex mt-1">
           <div className="text-black text-xl font-roboto tracking-wide flex gap-6 py-1 mr-6">
-            <Link className="" to="/recommend">
-              <p className="py-2 mr-2">Recommend</p>
+            <Link className="" to="/summarise">
+              <p className="py-2 mr-2">Summarise</p>
             </Link>
             <Link className="" to="/resources">
-              <p className="mr-2 py-2">Resources</p>
+              <p className="mr-2 py-2"></p>
             </Link>
             <Link className="" to="/contact">
               <p className="mr-2 py-2">Contact Us</p>
